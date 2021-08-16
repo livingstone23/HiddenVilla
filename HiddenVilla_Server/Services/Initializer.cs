e1058fda981cb4d3fb9d6da84829cac3f73ba4bb
livingstone23 <livingstone23@gmail.com>
2021-08-16 15:31:05 +0200

    113 De Initializer

---
+++ HiddenVilla_Server/Services/Initializer.cs
@@ -0,0 +1,64 @@
+﻿using Common;
+using DataAccess.Data;
+using HiddenVilla_Server.Services.IService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Services
+{
+    public class Initializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public Initializer(ApplicationDbContext db, UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;    
+        }
+
+
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            if (_db.Roles.Any(x => x.Name == SD.Role_Admin)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            IdentityUser user = _db.Users.FirstOrDefault(u => u.Email == "admin@gmail.com");
+            _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+
+        }
+
+
+    }
+}
